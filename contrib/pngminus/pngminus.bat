1562479823697a95e97c5e3b16c861dbb554dfb0
Cosmin Truta <ctruta@gmail.com>
2019-01-27 22:56:44 -0500

    pngminus: Improve portability of Makefile; fix test drivers

--- contrib/pngminus/pngminus.bat
+++ contrib/pngminus/pngminus.bat
@@ -1,4 +1,8 @@
 make
+copy png2pnm-shared.exe png2pnm.exe
+REM copy png2pnm-static.exe png2pnm.exe
+copy pnm2png-shared.exe pnm2png.exe
+REM copy pnm2png-static.exe pnm2png.exe
 call png2pnm.bat
 call pnm2png.bat
 
--- contrib/pngminus/pngminus.sh
+++ contrib/pngminus/pngminus.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 make
+cp -p png2pnm-shared png2pnm
+# cp -p png2pnm-static png2pnm
+cp -p pnm2png-shared pnm2png
+# cp -p pnm2png-static pnm2png
 sh png2pnm.sh
 sh pnm2png.sh
 
